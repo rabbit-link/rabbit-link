b10400488fc25f88f1a2f049faceea6bc4e9b003
Artur Kraev <ravenox@gmail.com>
2015-10-23 18:32:19 +0300

    Build script update

--- build.cmd
+++ build.cmd
@@ -1,7 +1,13 @@
 md %~dp0artifacts
 
+%~dp0util\nuget.exe restore %~dp0src\RabbitLink.sln
+
+@if %errorlevel% neq 0 ( pause & exit /b %errorlevel%)
+
 "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" /m /p:Configuration=Release "src\RabbitLink.sln"
 
-@if %errorlevel% equ 0 (%~dp0util\nuget.exe pack %~dp0src\RabbitLink\RabbitLink.csproj -OutputDirectory %~dp0artifacts -Properties Configuration=Release)
+@if %errorlevel% neq 0 ( pause & exit /b %errorlevel%)
+
+%~dp0util\nuget.exe pack %~dp0src\RabbitLink\RabbitLink.csproj -OutputDirectory %~dp0artifacts -Properties Configuration=Release
 
 pause
