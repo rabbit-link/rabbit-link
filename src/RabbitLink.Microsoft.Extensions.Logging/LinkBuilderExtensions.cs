cc3af6275157d7135ad71df05861afcb21ce6427
ijsgaus <ijsgaus@gmail.com>
2020-09-29 18:19:39 +0300

    Add ILinkLogger implementation for Microsoft.Extensions.Logging.Abstractions Add ILinkSerializer implementation for System.Text.Json.JsonSerializer

---
+++ src/RabbitLink.Microsoft.Extensions.Logging/LinkBuilderExtensions.cs
@@ -0,0 +1,19 @@
+using RabbitLink.Builders;
+using Microsoft.Extensions.Logging;
+
+namespace RabbitLink.Logging
+{
+    public static class LinkBuilderExtensions
+    {
+        /// <summary>
+        /// Use <see cref="Microsoft.Extensions.Logging.ILoggerFactory"/> as ILinkLogger
+        /// </summary>
+        /// <param name="builder">Link builder</param>
+        /// <param name="factory"><see cref="Microsoft.Extensions.Logging.ILoggerFactory"/> to use</param>
+        /// <param name="categoryPrefix">prefix the category of RabbitLink logging messages</param>
+        /// <returns></returns>
+        public static ILinkBuilder UseMicrosoftExtensionsLogging(this ILinkBuilder builder, ILoggerFactory factory,
+            string categoryPrefix = "RabbitLink.")
+            => builder.LoggerFactory(new LoggerFactory(factory, categoryPrefix));
+    }
+}
---
+++ src/RabbitLink.Microsoft.Extensions.Logging/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace RabbitLink.Logging
+{
+    internal class Logger : ILinkLogger
+    {
+        private readonly ILogger _logger;
+
+        public Logger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void Write(LinkLoggerLevel level, string message)
+        {
+            switch (level)
+            {
+                case LinkLoggerLevel.Error:
+                    _logger.LogError(message);
+                    break;
+                case LinkLoggerLevel.Warning:
+                    _logger.LogWarning(message);
+                    break;
+                case LinkLoggerLevel.Info:
+                    _logger.LogInformation(message);
+                    break;
+                case LinkLoggerLevel.Debug:
+                    _logger.LogDebug(message);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(level), level, null);
+            }
+        }
+    }
+}
